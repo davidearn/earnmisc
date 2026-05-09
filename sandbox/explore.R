acc1ffb0bf4f34eab8d807f0363fa86e81618edc
David Earn <earn@math.mcmaster.ca>
2026-05-09 13:01:39 -0700

    update

--- sandbox/explore.R
+++ sandbox/explore.R
@@ -2,11 +2,14 @@ rs()
 library(earnmisc)
 ?earnmisc
 plot(0,0)
-xys_line(0, 1, 2)
+(xys_line(0, 1, 2))
+oi.blue.2 <- oi_colour("blue", alpha = 0.2)
 xys.out <- xys_line(c(0,0.05), c(0.1,-0.1), c(1,-0.5)
-         , col = c("blue","red")
+         , col = c("blue","red",oi.blue.8)
          , lty = c("solid","dotted","dashed")
-         , lwd = c(3,1)
+         , lwd = c(4,2,1)
          )
 xys.out
 example(xys_line)
+?okabe_ito_colours
+oi.orange
